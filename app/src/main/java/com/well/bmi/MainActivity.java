0684cb6d73b4e6324a5a7888433a9a9562aaaa9e
ax013944 <ax013944@gmail.com>
2018-10-02 16:31:33 +0800

    toString

--- app/src/main/java/com/well/bmi/MainActivity.java
+++ app/src/main/java/com/well/bmi/MainActivity.java
@@ -1,7 +1,11 @@
 package com.well.bmi;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.text.Editable;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+        public void bmi(View view){
+            Log.d("MainActivity", "bmi");
+            EditText ed_weight = findViewById(R.id.ed_weight);
+            EditText ed_height = findViewById(R.id.ed_height);
+            String w =ed_weight.getText().toString();
+            String h =ed_height.getText().toString();
+            Log.d("MainActivity", w+"/"+h);
+            float weight= Float.parseFloat(w);
+            float height= Float.parseFloat(h);
+            float bmi = weight/(height*height);
+            Log.d("MainActivity", bmi+"");
+        }
+    }
+
